80445cb08453a19660351c926fbcfb917ca1dd63
dongzhao <41535479+Naoketong@users.noreply.github.com>
2020-03-31 14:10:02 +0800

    all

--- src/pages/over.vue
+++ src/pages/over.vue
@@ -1,45 +1,23 @@
 <template>
-    <Layout>
-        <div class="block">
-    <span class="demonstration" @click="qq">默认</span>
-    <el-date-picker
-      v-model="value"
-      
-      type="daterange"
-      range-separator="至"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期"
-      >
-    </el-date-picker>
-    </br>
-  </div>
- 
-  
-
-
-    </Layout>
+	<Layout>
+			
+	</Layout>
 
 </template>
 <script>
-    import Layout from '@/components/Layout'
+	import Layout from '@/components/Layout'
 
-    export default {
-        data() {
-            return{
-                
-        value: '',
-            }
-        },
-        created(){
-            console.log()
-        },
-        methods:{
-            qq(){
-                console.log(this.value)
-            },
-        },
-        components: {
-            Layout,
-        },
-    }
+	export default {
+		data() {
+			return{
+			}
+		},
+		created(){
+		},
+		methods:{
+		},
+		components: {
+			Layout,
+		},
+	}
 </script>
